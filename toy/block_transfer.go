c774fe34376e1bc1a8ae237205e720c4157b67c0
Tej Chajed <tchajed@mit.edu>
2020-07-03 19:58:11 -0500

    Factor out spawned thread in toy example

--- toy/block_transfer.go
+++ toy/block_transfer.go
@@ -2,6 +2,21 @@ package toy
 
 import "github.com/tchajed/goose/machine/disk"
 
+// assumes a crash invariant with a guarantee that says a has an even block (the
+// initial status doesn't matter); this should be sufficient to prove that this
+// function does not panic
+func consumeEvenBlock(d disk.Disk, a uint64) {
+	b4 := make(disk.Block, disk.BlockSize)
+	b4[0] = 4
+	d.Write(a, b4)
+	b := d.Read(a)
+	if b[0] != 4 {
+		// the proof will show this does not happen (which would otherwise
+		// get stuck in the semantics)
+		panic("unexpected value on disk")
+	}
+}
+
 // TransferEvenBlock assumes it is given ownership of a and that a initially has
 // an even block (defined as the first byte being even).
 //
@@ -12,14 +27,6 @@ func TransferEvenBlock(d disk.Disk, a uint64) {
 
 	// logically transfer a to this function
 	go func() {
-		b4 := make(disk.Block, disk.BlockSize)
-		b4[0] = 4
-		d.Write(a, b4)
-		b := d.Read(a)
-		if b[0] != 4 {
-			// the proof will show this does not happen (which would otherwise
-			// get stuck in the semantics)
-			panic("unexpected value on disk")
-		}
+		consumeEvenBlock(d, a)
 	}()
 }
